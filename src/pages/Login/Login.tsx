5e70f0ec1ba7a88bd370eb98378f8ef540fc2969
NguyenThienAn <nguyenthienan@mobcec.com>
2023-02-18 11:43:50 +0700

    +Add: floating UI && framer-motion

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -63,7 +63,7 @@ export default function Register() {
 
   return (
     <div className='w-full rounded-lg bg-white shadow dark:border dark:border-gray-700 dark:bg-gray-800 md:mt-0 xl:p-0'>
-      <div className='space-y-4 p-6 sm:p-8 md:space-y-6'>
+      <div className='space-y-4 p-6 sm:p-8 md:space-y-5'>
         <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl'>
           Sign in to your account
         </h1>
